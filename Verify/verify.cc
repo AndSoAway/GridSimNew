0a9ee53e2d0b6dcb5c6fff1c1381d1b852d4de57
AndSoAway <xyqhello@gmail.com>
2015-12-10 15:33:37 +0800

    calculate the total pair

--- Verify/verify.cc
+++ Verify/verify.cc
@@ -13,7 +13,7 @@ double verify(const Trajectory& target, const Trajectory& candidate) {
 	double can2tr_min_dist = 0;
 	const vector<SamplePoint>& tr_point_list = target.point_list();
 	int n = tr_point_list.size();
-	printf("n is %d\n", n);
+//	printf("n is %d\n", n);
 	for (int index = 0; index < n; index++) {
 		double min_dis = minDistance(tr_point_list.at(index), candidate);	      
 //		printf("tar id %d, can id %d, min_dis %lf\n", target.id(), candidate.id(), min_dis);
@@ -28,7 +28,7 @@ double verify(const Trajectory& target, const Trajectory& candidate) {
 		can2tr_min_dist += min_dis;
 	}
 	
-	printf("DISUNIT is %d\n", DISUNIT);
+//	printf("DISUNIT is %d\n", DISUNIT);
 	double sim = 1 - (tr2can_min_dist + can2tr_min_dist) / ((double)(n + m) * DISUNIT);	
 //	printf("get tr2can_min_dist %lf\n", tr2can_min_dist);
 //	printf("get can2tr_min_dist %lf\n", can2tr_min_dist);
