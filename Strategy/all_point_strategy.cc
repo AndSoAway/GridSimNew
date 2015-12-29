0d86c0cd448b7e33873c1a9abcd6245df814b7c8
AndSoAway <xyqhello@gmail.com>
2015-12-29 15:59:23 +0800

    change the way of GetCandidateTrajs

--- Strategy/all_point_strategy.cc
+++ Strategy/all_point_strategy.cc
@@ -7,13 +7,13 @@ void AllPointStrategy::FindCandidateTrajs(const GridPanel* grid_panel, const Tra
 	int point_size = point_list.size();
 
 	const PointInfo& chose_point = point_list[0];
-	const pair<int, int>& grid_index = chose_point.grid_index_;
-	candidates = grid_panel->panel().GetTrajsAroundGrid(grid_index);
-	
+//	const pair<int, int>& grid_index = chose_point.grid_index_;
+//	candidates = grid_panel->panel().GetTrajsAroundGrid(grid_index);
+	GetCandidateTrajs(grid_panel, chose_point, dis, candidates, false);	
 	for(int i = 1; i < point_size; i++) {
 		const PointInfo& chose_point = point_list[i];
 
-		const list<int>& cur_can;
+		list<int> cur_can;
 		list<int>  res;
 		GetCandidateTrajs(grid_panel, chose_point, dis, cur_can, false);
 		TrajMergeJoin(candidates, cur_can, res);
