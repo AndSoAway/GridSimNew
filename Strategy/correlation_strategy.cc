21af2052df6b127554fca5eab16f914a77d0aed0
AndSoAway <xyqhello@gmail.com>
2015-12-31 16:28:29 +0800

    is_first should be set false

--- Strategy/correlation_strategy.cc
+++ Strategy/correlation_strategy.cc
@@ -7,14 +7,14 @@ void CorrelationStrategy::FindCandidateTrajs(const GridPanel* grid_panel, const 
 	const vector<PointInfo>& point_list = traj.pointinfo_list();	
 	int point_size = point_list.size();
 	
-	pair<int, int> *pre_grid, *cur_grid;
+	const pair<int, int> *pre_grid, *cur_grid;
 //	pair<int, int> *min_grid;
 	int min_size_point;
 	const RelationInfo *pre_relation, *cur_relation;
 	//const RelationInfo *min_relation;
 	int min_tra_count; 
 	
-	PointInfo& first_point = point_list[0];
+	const PointInfo& first_point = point_list[0];
 	pre_grid = &first_point.grid_index_;
 	pre_relation = grid_panel->panel().GetRelationInfo(*pre_grid);
 	
@@ -49,6 +49,7 @@ void CorrelationStrategy::FindCandidateTrajs(const GridPanel* grid_panel, const 
 			GetCandidateTrajs(grid_panel, point_list[min_size_point], dis, min_tra_list, false);
 			if (is_first) {
 				candidates = min_tra_list;
+				is_first = false;
 			}else {
 				list<int> res;
 				TrajMergeJoin(candidates, min_tra_list, res);
